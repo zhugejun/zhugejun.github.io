e297e1fd68ee3df913dcacb1a45bfcc466595666
Gejun Zhu <gejun.anderson@gmail.com>
2023-10-19 11:34:07 -0500

    add data science basics

---
+++ content/datasci-simplified/back-to-basics.md
@@ -0,0 +1,72 @@
+---
+title: "Back to Basics - Data Science"
+date: 2021-10-16
+draft: false
+---
+
+## Statistics
+
+- Introduction to Probability:
+- Probability Distributions:
+- Bayes Theorem:
+- Central Limit Theorem: [Code](https://github.com) | [Slides](/datasci-simplified/central-limit-theorem/index.html) | [Video](https://youtu.be/2QeDRsxSF9M)
+- Confidence Interval:
+- Hypothesis Testing:
+- p-value:
+- Type I and Type II Errors:
+- Power of a Test:
+- t-test:
+- ANOVA:
+- Chi-Square Test:
+- Linear Regression:
+
+## Machine Learning
+
+- Logistic Regression:
+- Decision Trees:
+- Random Forests:
+- Gradient Boosting:
+- Support Vector Machines:
+- K-Nearest Neighbors:
+- K-Means Clustering:
+- Hierarchical Clustering:
+- Principal Component Analysis:
+- Singular Value Decomposition:
+
+## Deep Learning
+
+- Neural Networks:
+- Convolutional Neural Networks:
+
+## Natural Language Processing
+
+- Word Embeddings:
+- Word2Vec:
+- GloVe:
+- FastText:
+- BERT:
+
+## LeetCode (Grind75)
+
+- [Two Sum](https://leetcode.com/problems/two-sum/)
+
+## R
+
+- tidyverse:
+- ggplot2:
+- dplyr:
+- tidyr:
+- purrr:
+- readr:
+- tibble:
+- stringr:
+
+## Python
+
+- numpy:
+- pandas:
+- matplotlib:
+- seaborn:
+- scikit-learn:
+- scipy:
+- decorator:
---
+++ content/datasci-simplified/utils.R
@@ -0,0 +1,105 @@
+library(tidyverse)
+library(gridExtra)
+library(kableExtra)
+library(knitr)
+
+
+is_red = function(x) x == red_marble
+
+get_samples = function(population, sample_size, number_of_samples = 1, func = NULL) {
+  
+  if (is.null(func)) {
+    func = function(x) x
+  }
+  
+  samples = tibble(sample_number = numeric(), sample_value = numeric())
+  for (i in c(1:number_of_samples)) {
+    sample_number = rep(i, each = sample_size)
+    sample_value = func(sample(population, sample_size))
+    samples = bind_rows(samples, tibble(sample_number, sample_value))
+  }
+  
+  return(samples)
+}
+
+plot_samples_and_averages = function(samples, true_value) {
+  
+  x_min = floor(min(samples$sample_value))
+  x_max = ceiling(max(samples$sample_value))
+
+  # split x to 10 bins
+  x_breaks = seq(x_min, x_max, (x_max - x_min) / 10)
+  
+  # Calculating average of red marbles in each sample
+  sample_means = samples |>
+    group_by(sample_number) |>
+    summarise(average = mean(sample_value))
+  
+  # plotting samples as points and sample means 
+  p1 = ggplot(samples, aes(x = sample_value, y = sample_number)) +
+    geom_jitter(aes(color = factor(sample_value)), width = 0.2, height = 0) +
+    geom_point(data = sample_means, aes(x = average, y = sample_number), color = "orange", size = 2) +
+    geom_path(data = sample_means, aes(x = average, y = sample_number), color = "orange") +
+    scale_color_manual(values = c("1" = "red", "0" = "blue")) +
+    scale_y_continuous(breaks = seq(0, max(samples$sample_number), 5)) +
+    labs(y = "Sample #", x = "Sample Value") +
+    theme_minimal() + 
+    theme(legend.position = "none")
+  
+  # Plotting histogram of sample means
+  p2 = ggplot(sample_means, aes(x = average)) +
+    geom_histogram(aes(y = after_stat(density)), fill = "orange", color = "white", breaks = seq(x_min, x_max, (x_max - x_min) / 50)) +
+    geom_density(aes(y = after_stat(density)), color = "red") +
+    scale_x_continuous(limits = c(x_min, x_max), breaks = seq(x_min, x_max, (x_max - x_min) / 10)) +
+    geom_vline(xintercept = true_value, color = "blue", linetype = "dashed") +
+    labs(x = "Sample mean", y = "Density", title = "Sampling Distribution of the Sample Means") +
+    theme_minimal()
+  
+  # Combining the plots vertically
+  grid.arrange(p1, p2, ncol = 1)
+  
+}
+
+
+plot_samples = function(samples) {
+
+  sample_means = samples |>
+    group_by(sample_number) |>
+    summarise(average = mean(sample_value))
+
+  p1 = ggplot(samples, aes(x = sample_value, y = sample_number)) +
+    geom_jitter(aes(color = factor(sample_value)), width = 0.2, height = 0) +
+    geom_point(data = sample_means, aes(x = average, y = sample_number), color = "orange", size = 2) +
+    geom_path(data = sample_means, aes(x = average, y = sample_number), color = "orange") +
+    scale_color_manual(values = c("1" = "red", "0" = "blue")) +
+    scale_y_continuous(breaks = seq(0, max(samples$sample_number), 5)) +
+    labs(y = "Sample #", x = "Sample Value") +
+    theme_minimal() + 
+    theme(legend.position = "none")
+  
+  return(p1)
+}
+
+plot_sample_means = function(samples, true_prop) {
+
+  x_min = floor(min(samples$sample_value))
+  x_max = ceiling(max(samples$sample_value))
+
+  x_breaks = seq(x_min, x_max, (x_max - x_min) / 10)
+  
+  sample_means = samples |>
+    group_by(sample_number) |>
+    summarise(average = mean(sample_value))
+  
+  p2 = ggplot(sample_means, aes(x = average)) +
+    geom_histogram(aes(y = after_stat(density)), fill = "orange", color = "white", breaks = seq(x_min, x_max, (x_max - x_min) / 50)) +
+    geom_density(aes(y = after_stat(density)), color = "red") +
+    geom_vline(xintercept = true_prop, color = "blue", linetype = "dashed") +
+    scale_x_continuous(limits = c(x_min, x_max), breaks =  seq(x_min, x_max, (x_max - x_min) / 10)) +
+    labs(x = "Sample mean", y = "Density") +
+    ylim(0, 11.0) +
+    theme_minimal() + 
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  return(p2)
+}
